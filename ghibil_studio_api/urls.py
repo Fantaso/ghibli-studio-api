cce22860b183191a39d080073d1cf74aeb59c0ff
fantaso <fantaso.code@gmail.com>
2019-11-22 11:35:41 +0100

    added url path /movies

--- ghibil_studio_api/urls.py
+++ ghibil_studio_api/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from movies.views import MoviesHomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('movies/', MoviesHomeView.as_view(), name='movies_page'),
 ]
